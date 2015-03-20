f60f1d5951851a9c7d5c70b759878576231194b8
Michael Yenik <mgyenik@ncsu.edu>
2015-03-20 00:04:31 -0700

    DAC: straight import of DAC-DMA example from libopencm3-examples

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -n "$1" ]
+then
+    echo "Usage: $0 object.bin"
+    exit $E_BADARGS
+fi
+
+st-flash write $1 0x8000000
+exit 0
--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencmsis/core_cm3.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/dac.h>
+#include <libopencm3/stm32/dma.h>
 
 #include "cdcacm.h"
 #include "ring.h"
@@ -25,8 +29,14 @@
 #define IAMP        1000
 #define QAMP        1000
 
+#define PERIOD 1152
+
 struct ring input_ring;
 uint8_t input_ring_buffer[BUFFER_SIZE];
+
+/* Globals */
+uint8_t waveform[256];
+
 
 /*******************************************************
  * My structs for the different types of data packets! *
@@ -112,20 +122,131 @@ static void generate_baseband(union txdata *output, struct IQdata *BBdata){
 	(void)BBdata;
 }
 
+static void timer_setup(void)
+{
+	/* Enable TIM2 clock. */
+	rcc_periph_clock_enable(RCC_TIM2);
+	timer_reset(TIM2);
+	/* Timer global mode: - No divider, Alignment edge, Direction up */
+	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT,
+		       TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+	timer_continuous_mode(TIM2);
+	timer_set_period(TIM2, PERIOD);
+	timer_disable_oc_output(TIM2, TIM_OC2 | TIM_OC3 | TIM_OC4);
+	timer_enable_oc_output(TIM2, TIM_OC1);
+	timer_disable_oc_clear(TIM2, TIM_OC1);
+	timer_disable_oc_preload(TIM2, TIM_OC1);
+	timer_set_oc_slow_mode(TIM2, TIM_OC1);
+	timer_set_oc_mode(TIM2, TIM_OC1, TIM_OCM_TOGGLE);
+	timer_set_oc_value(TIM2, TIM_OC1, 500);
+	timer_disable_preload(TIM2);
+	/* Set the timer trigger output (for the DAC) to the channel 1 output
+	   compare */
+	timer_set_master_mode(TIM2, TIM_CR2_MMS_COMPARE_OC1REF);
+	timer_enable_counter(TIM2);
+}
+
+static void dac_setup(void)
+{
+	/* Enable the DAC clock on APB1 */
+	rcc_periph_clock_enable(RCC_DAC);
+	/* Setup the DAC channel 1, with timer 2 as trigger source.
+	 * Assume the DAC has woken up by the time the first transfer occurs */
+	dac_trigger_enable(CHANNEL_1);
+	dac_set_trigger_source(DAC_CR_TSEL1_T2);
+	dac_dma_enable(CHANNEL_1);
+	dac_enable(CHANNEL_1);
+}
+
+void dma1_stream5_isr(void)
+{
+	if (dma_get_interrupt_flag(DMA1, DMA_STREAM5, DMA_TCIF)) {
+		dma_clear_interrupt_flags(DMA1, DMA_STREAM5, DMA_TCIF);
+		/* Toggle PC1 just to keep aware of activity and frequency. */
+		gpio_toggle(GPIOC, GPIO1);
+	}
+}
+
+static void dma_setup(void)
+{
+	/* DAC channel 1 uses DMA controller 1 Stream 5 Channel 7. */
+	/* Enable DMA1 clock and IRQ */
+	rcc_periph_clock_enable(RCC_DMA1);
+	nvic_enable_irq(NVIC_DMA1_STREAM5_IRQ);
+	dma_stream_reset(DMA1, DMA_STREAM5);
+	dma_set_priority(DMA1, DMA_STREAM5, DMA_SxCR_PL_LOW);
+	dma_set_memory_size(DMA1, DMA_STREAM5, DMA_SxCR_MSIZE_8BIT);
+	dma_set_peripheral_size(DMA1, DMA_STREAM5, DMA_SxCR_PSIZE_8BIT);
+	dma_enable_memory_increment_mode(DMA1, DMA_STREAM5);
+	dma_enable_circular_mode(DMA1, DMA_STREAM5);
+	dma_set_transfer_mode(DMA1, DMA_STREAM5,
+				DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+	/* The register to target is the DAC1 8-bit right justified data
+	   register */
+	dma_set_peripheral_address(DMA1, DMA_STREAM5, (uint32_t) &DAC_DHR8R1);
+	/* The array v[] is filled with the waveform data to be output */
+	dma_set_memory_address(DMA1, DMA_STREAM5, (uint32_t) waveform);
+	dma_set_number_of_data(DMA1, DMA_STREAM5, 256);
+	dma_enable_transfer_complete_interrupt(DMA1, DMA_STREAM5);
+	dma_channel_select(DMA1, DMA_STREAM5, DMA_SxCR_CHSEL_7);
+	dma_enable_stream(DMA1, DMA_STREAM5);
+}
+
+static void clocks(void)
+{
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]); /* Turn down for what */
+
+	/* Port A and C are on AHB1 */
+	rcc_periph_clock_enable(RCC_GPIOA); /* For DAC output */
+	rcc_periph_clock_enable(RCC_GPIOC); /* For timing measure pin */
+
+	rcc_periph_clock_enable(RCC_GPIOD); /* For LEDs */
+}
+
+static void gpio_setup(void)
+{
+	// LED Stuff!!!
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	/* Set the digital test output on PC1 */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+	gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ, GPIO1);
+	/* Set PA4 for DAC channel 1 to analogue, ignoring drive mode. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO4);
+
+}
+
 int main(void)
 {
 	//u8 tx_buffer;
 	int rx_status, i;
-	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_120MHZ]);
 
 	union txdata txdata;
 
 	// Setup the ring buffer
 	ring_init(&input_ring, input_ring_buffer, BUFFER_SIZE);
 
-	// LED Stuff!!!
-	rcc_periph_clock_enable(RCC_GPIOD);
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	/* DAC test data */
+	uint16_t j, x;
+	for (j = 0; j < 256; j++) {
+		if (j < 10) {
+			x = 10;
+		} else if (j < 121) {
+			x = 10 + ((j*j) >> 7);
+		} else if (j < 170) {
+			x = j/2;
+		} else if (j < 246) {
+			x = j + (80 - j/2);
+		} else {
+			x = 10;
+		}
+		waveform[j] = x;
+	}
+
+	clocks();
+	gpio_setup();
+	timer_setup();
+	dma_setup();
+	dac_setup();
 
 	cdcacm_init(&input_ring);
 
